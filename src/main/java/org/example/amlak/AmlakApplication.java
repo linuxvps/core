891b3d5c554f51ec659d578d3c9a894da9aa80b2
nima <linux.vps.vpn@gmail.com>
2025-06-11 23:31:50 +0330

    init

---
+++ src/main/java/org/example/amlak/AmlakApplication.java
@@ -0,0 +1,13 @@
+package org.example.amlak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmlakApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmlakApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/amlak/AmlakApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.amlak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmlakApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
